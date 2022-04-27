3779efae07f460048880f6f607a6efe7c8ce9f90
JBWIL <89558708+JBWIL@users.noreply.github.com>
2022-04-27 00:21:52 -0400

    Music Implemented, Levels Designed, Level Select Script Fixed

--- Beat Box/Assets/Scripts/Menu.cs
+++ Beat Box/Assets/Scripts/Menu.cs
@@ -7,4 +7,15 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void StartLevelOne() {
+      SceneManager.LoadScene("Level 1");
+    }
+
+    public void StartLevelTwo() {
+      SceneManager.LoadScene("Level 2");
+    }
+
+    public void StartLevelThree() {
+      SceneManager.LoadScene("Level 3");
+    }
 }
